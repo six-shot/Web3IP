1af23b080aa0838cb365028ac9bfac39fc580a99
six-shot <okhuomonajayi54@gmail.com>
2024-07-18 14:45:15 +0000

    fix

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
+var cron = require("node-cron");
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
-
 app.use(cors());
+
+cron.schedule("*/5 * * * *", () => {
+  console.log("See you next 5 minutes");
+});
+
+app.set("trust proxy", true);
 
 app.get("/api/ip", (req, res) => {
   const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
@@ -13,5 +18,5 @@ app.get("/api/ip", (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on ",process.env.PORT);
+  console.log("Server is running on ", process.env.PORT);
 });
